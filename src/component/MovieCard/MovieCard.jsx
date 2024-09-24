8ae148bd56a2189cc98e7deb0b18060e1655e6ff
dev-Nahian <nahian.bdcalling@gmail.com>
2024-09-24 15:28:42 +0600

    Title Added

--- src/component/MovieCard/MovieCard.jsx
+++ src/component/MovieCard/MovieCard.jsx
@@ -2,10 +2,15 @@ import React from 'react'
 const IMAGE_BASE_URL="https://image.tmdb.org/t/p/original";
 
 const MovieCard = ({movie}) => {
+    console.log(movie);
     
   return (
+    <div className='hover:scale-115 transition-all duration-100 ease-linear cursor-pointer'>
+        <img src={IMAGE_BASE_URL+movie.poster_path}  className='w-[220px] rounded-lg hover:border-[3px] border-gray-500 shadow-lg shadow-black'/>
+
+        <h4 className='w-[210px] text-white mt-2'>{movie.title}</h4>
+    </div>
    
-        <img src={IMAGE_BASE_URL+movie.poster_path}  className='w-[220px] rounded-lg hover:border-[3px] border-gray-500 hover:scale-110  transition-all duration-100 ease-linear cursor-pointer shadow-lg shadow-black'/>
   )
 }
 
